2f9ed509756df996ba9e7cda55abfc47d2d4cc5b
Charles726 <charles_726@hotmail.com>
2023-07-17 01:25:29 -0300

    atualização

---
+++ tiposdedados.js
@@ -0,0 +1,15 @@
+//vamos falar de tipo de dados primitivos e daremos alguns exemplos.
+
+const nome = "Luis"; // String.
+const nome1 = 'Luis'; // String.
+const nome2 = `Luis`; // String.
+const num1 = 10; // Number.
+const num2 = 10.52; // Number.
+
+let nomeAluno;// Undefined -> Não aponta para local nenhum na memória.
+let sobrenomeAluno = null; // Nulo -> Não aponta para lugar nenhum na memória.
+
+/*
+undefined -> É totalmente diferente de null.
+null -> É usado quando queremos anular uma variável
+*/
